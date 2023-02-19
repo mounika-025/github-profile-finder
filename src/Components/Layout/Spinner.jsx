356679bb8394c225cffb7b1b9c7f1868d6466ce9
mouni025 <im.mounika025@gmail.com>
2023-02-19 21:44:31 +0530

    added spinner

--- src/Components/Layout/Spinner.jsx
+++ src/Components/Layout/Spinner.jsx
@@ -1,11 +1,11 @@
-import CircleLoader from "react-spinners/CircleLoader";
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 
 function Spinner(){
     return(
-        <div className="w-100 mt-20">
-            <CircleLoader className="text-center mx-auto"/>
+        <div className="w-100 mt-20 flex justify-center">
+            <ClipLoader className="text-center mx-auto gray"/>
 
         </div>
     )
